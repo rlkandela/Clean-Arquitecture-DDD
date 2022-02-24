2d957835904261c65597951341e2b40f781a1d30
rlkandela <rlkandela@gmail.com>
2022-02-24 12:48:14 +0100

    finished chapter 33

---
+++ CleanArquitecture.Application/Contracts/Infrastructure/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using CleanArquitecture.Application.Models;
+
+namespace CleanArquitecture.Application.Contracts.Infrastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
---
+++ CleanArquitecture.Application/Contracts/Persistence/IVideoRepository.cs
@@ -0,0 +1,9 @@
+﻿using CleanArquitecture.Domain;
+
+namespace CleanArquitecture.Application.Contracts.Persistence
+{
+    public interface IVideoRepository : IAsyncRepository<Video>
+    {
+        Task<IEnumerable<Video>> GetVideoByName(string nombreVideo);
+    }
+}
