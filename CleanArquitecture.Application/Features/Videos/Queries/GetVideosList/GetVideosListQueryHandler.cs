53b347d509676ed0e8c2861b9c7c4314a9ab38a8
rlkandela <rlkandela@gmail.com>
2022-02-24 15:12:47 +0100

    missing solving compilation error

---
+++ CleanArquitecture.Application/Features/Videos/Queries/GetVideosList/GetVideosListQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CleanArquitecture.Application.Contracts.Persistence;
+using MediatR;
+
+namespace CleanArquitecture.Application.Features.Videos.Queries.GetVideosList
+{
+    public class GetVideosListQueryHandler : IRequestHandler<GetVideosListQuery, IEnumerable<VideosVM>>
+    {
+        private readonly IVideoRepository _videoRepository;
+        private readonly IMapper _mapper;
+
+        public GetVideosListQueryHandler(IVideoRepository videoRepository, IMapper mapper)
+        {
+            _videoRepository = videoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<VideosVM>> Handle(GetVideosListQuery request, CancellationToken cancellationToken)
+        {
+            var videoList = await _videoRepository.GetVideoByUsername(request.Username);
+
+            //return _mapper.Map<IEnumerable<VideosVM>>(videoList);
+            return new List<VideosVM>();
+        }
+    }
+}
---
+++ CleanArquitecture.Application/Features/Videos/Queries/GetVideosList/VideosVM.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArquitecture.Application.Features.Videos.Queries.GetVideosList
+{
+    internal class VideosVM
+    {
+        public string? Nombre { get; set; }
+
+        public int StreamerId { get; set; }
+    }
+}
