b7fc075aaf6a5dd453249e87a1dc62d6b15776f6
rlkandela <rlkandela@gmail.com>
2022-02-24 10:39:10 +0100

    finished chapter 22

--- CleanArquitecture.Domain/Common/BaseDomainModel.cs
+++ CleanArquitecture.Domain/Common/BaseDomainModel.cs
@@ -1,7 +1,19 @@
 ﻿namespace CleanArquitecture.Domain.Common
 {
-    public class BaseDomainModel
+    public abstract class BaseDomainModel
     {
         public int Id { get; set; }
+
+        #region Creation Data
+        public DateTime? DateCreated { get; set; }
+
+        public string? CreatedBy { get; set; }
+        #endregion
+
+        #region Modification Data
+        public DateTime? DateModified { get; set; }
+
+        public string? ModifiedBy { get; set; }
+        #endregion
     }
 }
