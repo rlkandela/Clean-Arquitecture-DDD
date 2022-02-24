3ccb5dc31c7960a2d7bd578c4f919ffceee7af98
rlkandela <rlkandela@gmail.com>
2022-02-24 08:56:49 +0100

    added new classes for actor and director

---
+++ CleanArquitecture.Domain/Actor.cs
@@ -0,0 +1,11 @@
+﻿namespace CleanArquitecture.Domain
+{
+    public class Actor
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+    }
+}
---
+++ CleanArquitecture.Domain/Director.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArquitecture.Domain
+{
+    public class Director
+    {
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+
+    }
+}
