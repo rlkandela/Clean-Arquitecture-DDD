ce11112595560e1a02f647963d17a6e3d5ae01ae
rlkandela <rlkandela@gmail.com>
2022-02-25 09:06:22 +0100

    added validations, finished chapter 36

---
+++ CleanArquitecture.Application/Features/Streamers/Commands/StreamerCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+
+namespace CleanArquitecture.Application.Features.Streamers.Commands
+{
+    public class StreamerCommandValidator : AbstractValidator<StreamerCommand>
+    {
+        public StreamerCommandValidator()
+        {
+            RuleFor(p => p.Nombre)
+                .NotEmpty().WithMessage("{Nombre} no puede estar en blanco")
+                .NotNull()
+                .MaximumLength(50).WithMessage("{Nombre} no puede exceder 50 caracteres");
+
+            RuleFor(p => p.Url)
+                .NotNull()
+                .NotEmpty().WithMessage("la {Url} no puede estar en blanco");
+        }
+    }
+}
