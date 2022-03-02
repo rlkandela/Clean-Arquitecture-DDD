eb5df916cf9f37fbf20d883205549c2029bef055
rlkandela <rlkandela@gmail.com>
2022-03-02 15:03:53 +0100

    partial commit

---
+++ CleanArquitecture.Infrastructure/Repositories/RepositoryBase.cs
@@ -0,0 +1,79 @@
+﻿using CleanArquitecture.Application.Contracts.Persistence;
+using CleanArquitecture.Domain.Common;
+using CleanArquitecture.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace CleanArquitecture.Infrastructure.Repositories
+{
+    public class RepositoryBase<T> : IAsyncRepository<T> where T : BaseDomainModel
+    {
+
+        protected readonly StreamerDbContext _context;
+
+        public RepositoryBase(StreamerDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> pred)
+        {
+            return await _context.Set<T>().Where(pred).ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>>? pred = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            string? includeString = null,
+            bool disableTracking = true
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+            if (!string.IsNullOrWhiteSpace(includeString)) query = query.Include(includeString);
+            if (pred != null) query = query.Where(pred);
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAsync(
+            Expression<Func<T, bool>>? pred = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+            List<Expression<Func<T, object>>>? includes = null,
+            bool disableTracking = true
+        )
+        {
+            IQueryable<T> query = _context.Set<T>();
+            if (disableTracking) query = query.AsNoTracking();
+            if (includes != null) includes.ForEach(include => query = query.Include(include));            
+            if (pred != null) query = query.Where(pred);
+            if (orderBy != null) return await orderBy(query).ToListAsync();
+            return await query.ToListAsync();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
