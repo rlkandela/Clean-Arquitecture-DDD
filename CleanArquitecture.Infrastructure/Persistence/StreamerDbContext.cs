1d0d207b241d1ff9dc93fd5cddcc70a1b64590d4
rlkandela <rlkandela@gmail.com>
2022-03-02 14:40:05 +0100

    finished chapter 46

---
+++ CleanArquitecture.Infrastructure/Persistence/StreamerDbContext.cs
@@ -0,0 +1,74 @@
+﻿using CleanArquitecture.Domain;
+using CleanArquitecture.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArquitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContext : DbContext
+    {
+        public StreamerDbContext(DbContextOptions<StreamerDbContext> options) : base(options)
+        {
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=PC-RAUL\\; Initial Catalog=Streamer;Integrated Security=True")
+        //        .LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, Microsoft.Extensions.Logging.LogLevel.Information)
+        //        .EnableSensitiveDataLogging();
+        //}
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<BaseDomainModel>())
+            {
+                switch(entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "system";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedDate = DateTime.Now;
+                        entry.Entity.ModifiedBy = "system";
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Streamer>()
+                .HasMany(m => m.Videos)
+                .WithOne(m => m.Streamer)
+                .HasForeignKey(m => m.StreamerId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Video>()
+                .HasMany(m => m.Actores)
+                .WithMany(m => m.Videos)
+                .UsingEntity<VideoActor>(
+                    m => m.HasKey(e => new { e.ActorId, e.VideoId })
+                );
+
+            modelBuilder.Entity<Director>()
+                .HasOne(m => m.Video)
+                .WithOne(m => m.Director)
+                .HasForeignKey<Director>(m => m.VideoId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
+        public DbSet<Streamer>? Streamers { get; set; }
+
+        public DbSet<Video>? Videos { get; set; }
+
+        public DbSet<VideoActor>? VideosActores { get; set; }
+
+        public DbSet<Actor>? Actores { get; set; }
+
+        public DbSet<Director>? Directores { get; set; }
+    }
+}
---
+++ CleanArquitecture.Infrastructure/Persistence/StreamerDbContextSeed.cs
@@ -0,0 +1,31 @@
+﻿using CleanArquitecture.Domain;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArquitecture.Infrastructure.Persistence
+{
+    public class StreamerDbContextSeed
+    {
+        public static async Task SeedAsync(StreamerDbContext context, ILogger<StreamerDbContext> logger)
+        {
+            await SeedStreamers(context, logger);
+        }
+
+        private static async Task SeedStreamers(StreamerDbContext context, ILogger<StreamerDbContext> logger)
+        {
+            if (context.Streamers!.Any()) return;
+            await context.Streamers!.AddRangeAsync(GetPreconfiguredStreamers());
+            await context.SaveChangesAsync();
+            logger.LogInformation($"Estamos insertando nuevos records al db {typeof(Streamer).Name}");
+            return;
+        }
+
+        private static IEnumerable<Streamer> GetPreconfiguredStreamers()
+        {
+            return new List<Streamer>
+            {
+                new Streamer {CreatedBy = "rlkandela", Nombre = "Maxi HBP", Url = "http://hbp.com"},
+                new Streamer {CreatedBy = "rlkandela", Nombre = "Amazon VIP", Url = "http://amazonvip.com"}
+            };
+        }
+    }
+}
