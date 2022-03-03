083f355f122af1c2b855c27fad23baa4c5f8117e
rlkandela <rlkandela@gmail.com>
2022-03-03 14:38:52 +0100

    finished section 15

---
+++ CleanArquitecture.API/Errors/CodeErrorResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace CleanArquitecture.API.Errors
+{
+    public class CodeErrorResponse
+    {
+        public int StatusCode { get; set; }
+        public string? Message { get; set; }
+
+        public CodeErrorResponse(int statusCode, string? message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageStatusCode(statusCode);
+        }
+
+        private string GetDefaultMessageStatusCode(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "El Request enviado tiene errores",
+                401 => "No tienes autorizacion para este recurso",
+                404 => "No se encontro el recurso solicitado",
+                500 => "Se produjeron errores en el servidor",
+                _ => string.Empty
+            };
+        }
+    }
+}
