c338e33a1b3bd14a3b0ab4884f3cda2001cf2f9c
rlkandela <rlkandela@gmail.com>
2022-03-03 13:03:20 +0100

    finished chapter 57, implemented registration and login

---
+++ CleanArquitecture.Identity/CleanArquitectureIdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using CleanArquitecture.Identity.Configurations;
+using CleanArquitecture.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArquitecture.Identity
+{
+    public class CleanArquitectureIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public CleanArquitectureIdentityDbContext(DbContextOptions<CleanArquitectureIdentityDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfiguration(new RoleConfiguration());
+            builder.ApplyConfiguration(new UserConfiguration());
+            builder.ApplyConfiguration(new UserRoleConfiguration());
+        }
+    }
+}
