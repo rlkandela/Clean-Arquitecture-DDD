c3bf7c171b486cdd1ff71d31bb5938825e024b62
rlkandela <rlkandela@gmail.com>
2022-03-03 12:10:31 +0100

    created new project, added references, finished chapter 55

---
+++ CleanArquitecture.Application/Models/Identity/RegistrationResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace CleanArquitecture.Application.Models.Identity
+{
+    public class RegistrationResponse
+    {
+        public string UserId { get; set; } = string.Empty;
+
+        public string UserName { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string Token { get; set; } = string.Empty;
+    }
+}
