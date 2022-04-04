bc836eee424268c6d61d669a725e12381d097e13
rlkandela <rlkandela@gmail.com>
2022-04-04 11:50:27 +0200

    finished section 17

--- CleanArquitecture.Application.UnitTests/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandHandlerXUnitTests.cs
+++ CleanArquitecture.Application.UnitTests/Features/Streamers/Commands/UpdateStreamer/UpdateStreamerCommandHandlerXUnitTests.cs
@@ -1,6 +1,47 @@
-﻿namespace CleanArquitecture.Application.UnitTests.Features.Streamers.Commands.UpdateStreamer
+﻿using AutoMapper;
+using CleanArquitecture.Application.Contracts.Infrastructure;
+using CleanArquitecture.Application.Features.Streamers.Commands.UpdateStreamer;
+using CleanArquitecture.Application.Mappings;
+using CleanArquitecture.Application.UnitTests.Mocks;
+using CleanArquitecture.Infrastructure.Repositories;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Shouldly;
+using Xunit;
+
+namespace CleanArquitecture.Application.UnitTests.Features.Streamers.Commands.UpdateStreamer
 {
     public class UpdateStreamerCommandHandlerXUnitTests
     {
+        private readonly IMapper _mapper;
+        private readonly Mock<UnitOfWork> _unitOfWork;
+        private readonly Mock<ILogger<UpdateStreamerCommandHandler>> _logger;
+
+        public UpdateStreamerCommandHandlerXUnitTests()
+        {
+            _unitOfWork = MockUnitOfWork.GetUnitOfWork();
+            MockStreamerRepository.AddDataStreamerRepository(_unitOfWork.Object.StreamerDbContext);
+
+            var mapperConfig = new MapperConfiguration(c => c.AddProfile<MappingProfile>());
+            _mapper = mapperConfig.CreateMapper();
+
+            _logger = new Mock<ILogger<UpdateStreamerCommandHandler>>();
+        }
+
+        [Fact]
+        public async Task UpdateStreamerCommand_InputStreamer_ReturnsUnit()
+        {
+            var streamerInput = new UpdateStreamerCommand()
+            {
+                Id = 9001,
+                Nombre = "NombreActualizado",
+                Url = "urlActualizada.com"
+            };
+
+            var handler = new UpdateStreamerCommandHandler(_unitOfWork.Object, _mapper, _logger.Object);
+            var result = await handler.Handle(streamerInput, CancellationToken.None);
+            result.ShouldBeOfType<Unit>();
+        }
     }
 }
